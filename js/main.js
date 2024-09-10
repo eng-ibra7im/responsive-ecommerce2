a07945789acf035185b8dc181e5a338174d58082
ibrahim hamed <pubg2652005@gmail.com>
2024-09-10 23:45:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let Mainimg = document.getElementById("Mainimg");
+let smallimg =document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function (){
+    Mainimg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function (){
+    Mainimg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function (){
+    Mainimg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function (){
+    Mainimg.src = smallimg[3].src;
+}
+
